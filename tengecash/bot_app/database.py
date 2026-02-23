efa451c0f0522659bf3d8d1a13bc583a0ff25e09
RustemYeldessov <eldesov1997@gmail.com>
2026-02-23 12:05:36 +0500

    refactor: Create database.py

---
+++ tengecash/bot_app/database.py
@@ -0,0 +1,71 @@
+from asgiref.sync import sync_to_async
+from django.contrib.auth import authenticate
+
+from tengecash.users.models import User
+from tengecash.categories.models import Category
+from tengecash.sections.models import Section
+from tengecash.expenses.models import Expense
+
+
+@sync_to_async
+def logout_user_db(tg_id):
+    user = User.objects.filter(telegram_id=tg_id).first()
+    if user:
+        user.telegram_id = None
+        user.save()
+        return True
+    return False
+
+@sync_to_async
+def get_user_by_tg_id(tg_id):
+    return User.objects.filter(telegram_id=tg_id).first()
+
+@sync_to_async
+def bind_user_with_password(tg_id, django_username, password):
+    user = authenticate(username=django_username, password=password)
+    if user is not None:
+        user.telegram_id = tg_id
+        user.save()
+        return f"Получилось! Ты вошел как пользователь {django_username}!"
+    return "Ошибка: Пользователь с таким именем не найден в базе данных"
+
+@sync_to_async
+def logout_user_db(tg_id):
+    user = User.objects.filter(telegram_id=tg_id).first()
+    if user:
+        user.telegram_id = None
+        user.save()
+        return True
+    return False
+
+@sync_to_async
+def get_categoies_db(user):
+    return list(Category.objects.filter(user=user))
+
+@sync_to_async
+def update_category_name(cat_id, new_name):
+    Category.objects.filter(id=cat_id).update(name=new_name)
+
+@sync_to_async
+def category_exists(user, name):
+    return Category.objects.filter(user=user, name__iexact=name).exists()
+
+@sync_to_async
+def create_category(user, name):
+    return Category.objects.create(user=user, name=name)
+
+@sync_to_async
+def category_delete(user, cat_id):
+    return Category.objects.filter(id=cat_id, user=user).delete()
+
+
+# @sync_to_async
+# def create_expense(user, section, category, amount, description, date):
+#     return Expense.objects.create(
+#         user=user,
+#         section=section,
+#         category=category,
+#         amount=amount,
+#         description=description,
+#         date=date
+#     )
