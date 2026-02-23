5e0a16d401d2940de87c802c98a75e4f7af39a0d
RustemYeldessov <eldesov1997@gmail.com>
2026-02-23 12:06:21 +0500

    refactor: Create states.py

---
+++ tengecash/bot_app/states.py
@@ -0,0 +1,127 @@
+from aiogram import F, Router
+from aiogram.types import Message, CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.fsm.state import State, StatesGroup
+from aiogram.fsm.context import FSMContext
+from asgiref.sync import sync_to_async
+
+from tengecash.bot_app.database import (
+    bind_user_with_password,
+    get_user_by_tg_id,
+    category_exists,
+    update_category_name,
+    create_category
+)
+
+router = Router()
+
+class LoginStates(StatesGroup):
+    waiting_for_username = State()
+    waiting_for_password = State()
+
+@router.message(LoginStates.waiting_for_username)
+async def process_username(message: Message, state: FSMContext):
+    await state.update_data(chosen_username=message.text)
+    await message.answer("Введи пароль:")
+    await state.set_state(LoginStates.waiting_for_password)
+
+@router.message(LoginStates.waiting_for_password)
+async def process_password(message: Message, state: FSMContext):
+    user_data = await state.get_data()
+    username = user_data['chosen_username']
+    password = message.text
+
+    await message.delete()
+    result = await bind_user_with_password(message.from_user.id, username, password)
+    await message.answer(result)
+    await state.clear()
+
+class CategoryEditStates(StatesGroup):
+    selecting_category = State()
+    remaining_category = State()
+    waiting_for_new_cat_name = State()
+
+@router.callback_query(F.data.startswith("edit_"))
+async def process_edit_category(callback: CallbackQuery, state: FSMContext):
+    cat_id = callback.data.split("_")[1]
+
+    from tengecash.bot_app.database import Category
+    category = await sync_to_async(Category.objects.filter(id=cat_id).first)()
+
+    await state.update_data(editing_cat_id=cat_id)
+
+    await callback.message.edit_text(
+        f"Введи новое название для этой категории <b>{category.name}</b>:",
+        parse_mode="HTML"
+    )
+    await state.set_state(CategoryEditStates.remaining_category)
+    await callback.answer()
+
+@router.message(CategoryEditStates.remaining_category)
+async def process_new_name(message: Message, state: FSMContext):
+    data = await state.get_data()
+    cat_id = data.get("editing_cat_id")
+    new_name = message.text.strip()
+
+    user = await get_user_by_tg_id(message.from_user.id)
+
+    if await category_exists(user, new_name):
+        await message.answer(
+            f"❌ Категория с именем <b>{new_name}</b> уже существует!\n"
+            "Введи другое название:",
+            parse_mode="HTML"
+        )
+        return
+
+    await update_category_name(cat_id, new_name)
+
+    await message.answer(
+        f"✅ Категория успешно переименована в: <b>{new_name}</b>",
+            parse_mode="HTML"
+    )
+    await state.clear()
+
+@router.message(CategoryEditStates.waiting_for_new_cat_name)
+async def process_add_category(message: Message, state: FSMContext):
+    new_cat_name = message.text.strip()
+    user = await get_user_by_tg_id(message.from_user.id)
+
+    if await category_exists(user, new_cat_name):
+        await message.answer(
+            f"❌ Категория с именем <b>{new_cat_name}</b> уже существует!\n"
+            "Введи другое название:",
+            parse_mode="HTML"
+        )
+        return
+
+    await create_category(user, new_cat_name)
+    await message.answer(
+        f"✅ Категория <b>{new_cat_name}</b> успешно создана!",
+            parse_mode="HTML"
+    )
+    await state.clear()
+
+@router.callback_query(F.data.startswith("delete_"))
+async def process_delete_category(callback: CallbackQuery):
+    cat_id = callback.data.split("_")[1]
+
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [
+            InlineKeyboardButton(text="Да, удалить ✅", callback_data=f"confirm_delete_{cat_id}"),
+            InlineKeyboardButton(text="Отмена ❌", callback_data=f"cancel_delete")
+        ]
+    ])
+
+    await callback.message.edit_text(
+        "⚠️ <b>Внимание!</b>\n"
+        "При удалении категории удалятся и все расходы, связанные с ней.\n"
+        "Ты уверен?",
+        reply_markup=keyboard,
+        parse_mode="HTML"
+    )
+    await callback.answer()
+
+# @router.message(ExpenseEditStates.waiting_for_new_expense_description)
+# async def process_expense_create(message: Message, state: FSMContext):
+#     expense_description = message.text.strip()
+#     expense_amount = int(message.text.strip())
+#     user = await get_user_by_tg_id(message.from_user.id)
