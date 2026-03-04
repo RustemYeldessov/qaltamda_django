57830fb3446cd0fa75b1045d604914be85cede9a
RustemYeldessov <eldesov1997@gmail.com>
2026-03-04 12:13:58 +0500

    feat: Add SOFT_DELETE_CASCADE in users/models.py

--- tengecash/users/models.py
+++ tengecash/users/models.py
@@ -1,9 +1,13 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, UserManager
 from django.utils.translation import gettext_lazy as _
+from safedelete.models import SafeDeleteModel, SOFT_DELETE_CASCADE
+from safedelete.managers import SafeDeleteManager
 
+class CustomUserManager(SafeDeleteManager, UserManager):
+    pass
 
-class User(AbstractUser):
+class User(AbstractUser, SafeDeleteModel):
     first_name = models.CharField(
         max_length=100,
         blank=False,
@@ -16,6 +20,12 @@ class User(AbstractUser):
     )
     USERNAME_FIELD = 'username'
     telegram_id = models.BigIntegerField(unique=True, null=True, blank=True)
+    _safedelete_policy = SOFT_DELETE_CASCADE
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
