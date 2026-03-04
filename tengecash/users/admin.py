1f65f43c049c634417309c7eec7a3634ef5ed3bf
RustemYeldessov <eldesov1997@gmail.com>
2026-03-04 12:12:39 +0500

    feat: Add class MyUserAdmin in admin.py

--- tengecash/users/admin.py
+++ tengecash/users/admin.py
@@ -1,2 +1,13 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from safedelete.admin import SafeDeleteAdmin, highlight_deleted
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class MyUserAdmin(SafeDeleteAdmin, UserAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'is_staff', 'highlight_deleted_field')
+
+    def highlight_deleted_field(self, obj):
+        return highlight_deleted(obj)
+    highlight_deleted_field.short_description = "Status"
