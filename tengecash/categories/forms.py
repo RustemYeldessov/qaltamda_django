3900d803a0281e78dc56c913d211cd7e6568e9a8
RustemYeldessov <eldesov1997@gmail.com>
2026-03-10 14:27:22 +0500

    feat: Add is_favorite to forms.py

--- tengecash/categories/forms.py
+++ tengecash/categories/forms.py
@@ -6,7 +6,8 @@ from .models import Category
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        fields = ['name', 'description']
+        fields = ['name', 'description', 'is_favorite']
         widgets = {
             'description': forms.Textarea(attrs={'rows': 4}),
+            'is_favorite': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
         }
